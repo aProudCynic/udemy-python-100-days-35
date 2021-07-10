f1490e51f6de18ee74eb3eaf67a81c6130321a25
Balazs Marton <balazs.marton@prezi.com>
2021-07-10 21:38:55 +0200

    feature: evaluate weather data

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
+import json
+
 import requests
 
 from const import (
     OPEN_WEATHER_API_BASE,
     OPEN_WEATHER_EXCLUDED_DATA,
+    MIN_WEATHER_CODE_TO_NOT_INDICATE_PRECIPITATION,
+    LENGTH_OF_HOURS_TO_EXAMINE,
+    HOUR_OF_NOTIFICATION,
 )
 from secrets import (
     HOME_LATITUDE,
     HOME_LONGITUDE,
     OPEN_WEATHER_API_KEY,
 )
+
+
+def fetch_weather_data():
+    response = requests.get(
+        url=OPEN_WEATHER_API_BASE,
+        params={
+            'lat': HOME_LATITUDE,
+            'lon': HOME_LONGITUDE,
+            'exclude': OPEN_WEATHER_EXCLUDED_DATA,
+            'appid': OPEN_WEATHER_API_KEY,
+        }
+    )
+    response.raise_for_status()
+    return response.json()
+
+def umbrella_is_needed(hourly_data):
+    for hour in range(HOUR_OF_NOTIFICATION, HOUR_OF_NOTIFICATION + LENGTH_OF_HOURS_TO_EXAMINE):
+        examined_hourly_data = hourly_data[hour]
+        weather_data = examined_hourly_data['weather']
+        for weather_details in weather_data:
+            if weather_details['id'] < MIN_WEATHER_CODE_TO_NOT_INDICATE_PRECIPITATION:
+                return True
+    return False
+
 
 response = requests.get(
     url=OPEN_WEATHER_API_BASE,
@@ -20,4 +49,6 @@ response = requests.get(
     }
 )
 response.raise_for_status()
-print(response.content)
+response_content = response.json()
+hourly_data = response_content['hourly']
+print('Bring an umbrella' if umbrella_is_needed(hourly_data) else 'No need for umbrella')
